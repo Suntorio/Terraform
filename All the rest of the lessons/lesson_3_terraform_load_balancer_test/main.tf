e87750d65fd699dfddce894f690259fa069008ca
Suntorio <suntorio80@gmail.com>
2023-10-08 20:38:41 -0700

    updates

--- All the rest of the lessons/lesson_3_terraform_load_balancer_test/main.tf
+++ All the rest of the lessons/lesson_3_terraform_load_balancer_test/main.tf
@@ -15,8 +15,8 @@ terraform {
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = var.key_id
-  secret_key = var.key_value
+  #access_key = var.key_id
+  #secret_key = var.key_value
 }
 
 #resource "tls_private_key" "test_key" {
